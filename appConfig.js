f6c55562183890a37636d554a04cdc76313d4a98
rajeskabol <rajeskabol@gmail.com>
2024-12-15 16:49:53 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "Coloring Me Free Coloring Page",
-  description: "Coloring Me Free Coloring Pag.",
-  keywords: "coloring,coloringkids,agc,cdom blog",
-  author: "Coloringme.biz.id",
-  favicon: "https://pugjs.org/images/favicon.ico",
+  title: "Rezept Lauchsuppe Vegan",
+  tagline: "Diese vegane Käse-Lauch-Suppe mit Hackfleisch ist schnell und einfach zubereitet und absolut lecker!",
+  description: "Mit einfachen Zutaten wie Frühlingszwiebeln, Brühe, Gemüsecreme, veganem Hackfleisch und Gewürzen können Sie im Handumdrehen eine köstliche, cremige und sättigende Suppe zubereiten.",
+  keywords: "Lauchsuppe vegan Kokosmilch,Kartoffel-Lauch-Suppe vegan,Hack Lauch-Suppe vegan,vegane käse-lauch-suppe mit kartoffeln,Vegane Lauchsuppe ohne Kartoffeln,Vegane Lauchsuppe Chefkoch,kartoffel-lauch-suppe einfach,Kartoffel-Lauch-Suppe stückig",
+  author: "Milyader Muda",
+  favicon: "",
   imageCount: 20,
   removeBadWords: "porn,nude,sex",
 };
